00846893eaade8bb500831b87716c30bb7a00073
Kleber Nascimento <kleber.nascimento5@fatec.sp.gov.br>
2021-06-16 16:23:07 -0300

    Importando o arquivo

---
+++ src/main/java/br/gov/sp/fatec/RHSpring/RhSpringApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.RHSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/RHSpring/RhSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.RHSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
